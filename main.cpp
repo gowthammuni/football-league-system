4f9fc4c08437bf942d9b5cd3170022922c9fd477
gobinda lugun <gobinknightryder098@gmail.com>
2021-11-10 11:58:20 +0530

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,6 @@
 using namespace std;
 int main()
 {
-  int ans = 0;
   printf("HELLO WORLD!!!!!");
-  scanf("%d", &ans);
-  printf("%d", ans);
-  sdndjcnsjdnc
-      sdcjsdnjcsndc
-          dsjbcjsdbcjhbsdjhc
-              dscsbdchbhsdhc
-                  sdchsdbchbdshjch
-                      sacabshcbahcbhja return 0;
+  return 0;
 }
